dfc92cdc9ad57be2c4a2c8cf7758f67d68c42f33
shstyoo <shstyoo@gmail.com>
2016-01-23 13:01:25 -0500

    edited file added testcase

--- hackerrank/grid-search/hackerrank_the-grid-search.py
+++ hackerrank/grid-search/hackerrank_the-grid-search.py
@@ -1,18 +1,21 @@
 import sys
 
-t = int(input().strip())
+f = open('testcase.txt', 'r')
+f_ = f.readline()
+t = int(f_)
 for a0 in range(t):
-    R,C = input().strip().split(' ')
+    R,C = f_.strip().split(' ')
     R,C = [int(R),int(C)]
     G = []
     G_i = 0
+    print(a0)
     for G_i in range(R):
-        G_t = str(input().strip())
+        G_t = str(f_.strip())
         G.append(G_t)
-    r,c = input().strip().split(' ')
+    r,c = f_.strip().split(' ')
     r,c = [int(r),int(c)]
     P = []
     P_i = 0
     for P_i in range(r):
-        P_t = str(input().strip())
+        P_t = str(f_.strip())
         P.append(P_t)
