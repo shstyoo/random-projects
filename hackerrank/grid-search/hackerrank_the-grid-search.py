035998d23fe3a34fd35882cf9196462d05578cc8
shstyoo <shstyoo@gmail.com>
2016-01-23 13:40:25 -0500

    finished txt file uploading

--- hackerrank/grid-search/hackerrank_the-grid-search.py
+++ hackerrank/grid-search/hackerrank_the-grid-search.py
@@ -1,21 +1,11 @@
 import sys
 
+# Open up file
 f = open('testcase.txt', 'r')
-f_ = f.readline()
-t = int(f_)
-for a0 in range(t):
-    R,C = f_.strip().split(' ')
-    R,C = [int(R),int(C)]
-    G = []
-    G_i = 0
-    print(a0)
-    for G_i in range(R):
-        G_t = str(f_.strip())
-        G.append(G_t)
-    r,c = f_.strip().split(' ')
-    r,c = [int(r),int(c)]
-    P = []
-    P_i = 0
-    for P_i in range(r):
-        P_t = str(f_.strip())
-        P.append(P_t)
+
+# Instantiate List
+f_ = []
+
+# Iterate for line in file, insert into list from 0 -> EOF
+for line in f:
+    f_.insert(len(f_), line)
