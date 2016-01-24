cf47e90b3e28c6d03b362931e406f4bb5c0f879b
shstyoo <shstyoo@gmail.com>
2016-01-24 18:06:10 -0500

    finished grid search

--- hackerrank/grid-search/hackerrank_the-grid-search.py
+++ hackerrank/grid-search/hackerrank_the-grid-search.py
@@ -1,6 +1,5 @@
 import sys
 
-# Need to save current column buffer to make sure we get all the test cases
 # Need to make sure that at the EOF on list G, we don't go out of bounds for a runtime error
 
 t = int(input().strip())
@@ -20,21 +19,51 @@ for a0 in range(t):
         P_t = str(input().strip())
         P.append(P_t)
     # Algorithm Starts here
-    for line in G:
-        i = 0
-        while i < len(G):
-            i2 = 0
-            rcounter = 0
-            if P[i2] in G[i]:
-                while i2 < len(P):
-                    if P[i2] in G[i]:
-                        rcounter += 1
-                    i2 += 1
-                    i += 1
-                if(rcounter == r):
-                    break
-            i += 1
-    if(rcounter == r):
-        print("YES")
+    list_ans = []
+    for i in range(R):
+        if(P[0] in G[i]):
+            for j in range(C-c+1):
+                if(P[0]==G[i][j:j+c]):
+                    ans =1
+                    for k in range(1,r):
+                        if(R>i+k and P[k]==G[i+k][j:j+c]):
+                            ans = ans+1
+                    list_ans.append(ans)
+    answer = [i for i in list_ans if i==r]
+    if(len(answer)>0):
+        print "YES"
     else:
-        print("NO")
+        print "NO"
+
+    # Other Algorithm
+    # for line in G:
+    #     i = 0
+    #     while i < len(G):
+    #         i2 = 0
+    #         rcounter = 0
+    #         if P[i2] in G[i]:
+    #             i3 = i
+    #             colPos = G[i3].find(P[i2])
+    #             while i2 < len(P):
+    #                 if P[i2] in G[i3]:
+    #                     if colPos == G[i3].find(P[i2]):
+    #                         rcounter += 1
+    #                 if(G[i3].count(P[i2]) > 1):
+    #                     G.remove(P[i2])
+    #                     x = 0
+    #                     while x < c:
+    #                         G.insert(colPos + x, -1)
+    #                     if P[i2] in G[i3]:
+    #                         if colPos == G[i3].find(P[i2]):
+    #                             rcounter += 1
+    #                 i2 += 1
+    #                 i3 += 1
+    #                 if(i3 > len(G)):
+    #                     break;
+    #             if(rcounter == r):
+    #                 break
+    #         i += 1
+    # if(rcounter == r):
+    #     print("YES")
+    # else:
+    #     print("NO")
