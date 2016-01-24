22e5f94a7e604217ec16b6b4a6d15c164a471761
shstyoo <shstyoo@gmail.com>
2016-01-23 19:33:57 -0500

    Algorithm works, but not for certain test cases. Outlined what needs to be done in file

--- hackerrank/grid-search/hackerrank_the-grid-search.py
+++ hackerrank/grid-search/hackerrank_the-grid-search.py
@@ -1,11 +1,40 @@
 import sys
 
-# Open up file
-f = open('testcase.txt', 'r')
+# Need to save current column buffer to make sure we get all the test cases
+# Need to make sure that at the EOF on list G, we don't go out of bounds for a runtime error
 
-# Instantiate List
-f_ = []
-
-# Iterate for line in file, insert into list from 0 -> EOF
-for line in f:
-    f_.insert(len(f_), line)
+t = int(input().strip())
+for a0 in range(t):
+    R,C = input().strip().split(' ')
+    R,C = [int(R),int(C)]
+    G = []
+    G_i = 0
+    for G_i in range(R):
+        G_t = str(input().strip())
+        G.append(G_t)
+    r,c = input().strip().split(' ')
+    r,c = [int(r),int(c)]
+    P = []
+    P_i = 0
+    for P_i in range(r):
+        P_t = str(input().strip())
+        P.append(P_t)
+    # Algorithm Starts here
+    for line in G:
+        i = 0
+        while i < len(G):
+            i2 = 0
+            rcounter = 0
+            if P[i2] in G[i]:
+                while i2 < len(P):
+                    if P[i2] in G[i]:
+                        rcounter += 1
+                    i2 += 1
+                    i += 1
+                if(rcounter == r):
+                    break
+            i += 1
+    if(rcounter == r):
+        print("YES")
+    else:
+        print("NO")
