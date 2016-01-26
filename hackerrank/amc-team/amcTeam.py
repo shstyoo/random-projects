1f5a7c31030b0c338d9e984f5f53ff64f8e4f4a3
shstyoo <shstyoo@gmail.com>
2016-01-26 15:45:24 -0500

    added amc-team

---
+++ hackerrank/amc-team/amcTeam.py
@@ -0,0 +1,18 @@
+# # Given a list of N people who are attending and given that each person
+# can only know one topic, calculate the maximum number of topics a 2 person
+# team can know. Also find how many teams can know that maximum number of
+# topics. N = #ofpeople, M = #oftopics. The matrix is either 1 or 0 depending
+# on whether or not the person knows the topic
+
+# https://www.hackerrank.com/challenges/acm-icpc-team
+
+import sys
+
+n,m = input().strip().split(' ')
+n,m = [int(n),int(m)]
+topic = []
+topic_i = 0
+for topic_i in range(n):
+   topic_t = str(input().strip())
+   topic.append(topic_t)
+   # Algorithm starts here
--- hackerrank/diagonal-difference/diagonalDifference.py
+++ hackerrank/diagonal-difference/diagonalDifference.py
@@ -2,3 +2,12 @@
 # the sums of its diagonals
 
 # https://www.hackerrank.com/challenges/diagonal-difference
+
+import sys
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+   a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+   a.append(a_t)
+   # Algorithm starts here
