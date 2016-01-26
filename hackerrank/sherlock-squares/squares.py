2d98575e21f26f0d7e4efc7bde6451e9d4ce0e71
shstyoo <shstyoo@gmail.com>
2016-01-26 00:33:36 -0500

    finished sherlock squares

--- hackerrank/sherlock-squares/squares.py
+++ hackerrank/sherlock-squares/squares.py
@@ -1,7 +1,4 @@
 # Given two integers, calculate the nubmer of integers that are squares of other integers between them (inclusive)
-# Time complexity given extremely large integers may cause a problem with iterating through a loop using
-# brute force to solve the issue. Using a more elegent solution, such as searching only for the square numbers
-# may provide a better result. Or even searching through a list of square root numbers that are integers.
 
 # https://www.hackerrank.com/challenges/sherlock-and-squares
 
@@ -17,5 +14,12 @@ for a0 in range(t):
     for i in range(n,k+1):
         i = i ** 0.5
         if(int(i) == i):
-            counter += 1
+            while(True):
+                x = i ** 2
+                i += 1
+                if(x > k):
+                    break
+                else:
+                    counter += 1
+            break
     print(counter)
