79d2872f4cac8344afa1e6571697880cb3cb90b3
shstyoo <shstyoo@gmail.com>
2016-02-17 07:24:08 -0500

    added matrix rotation

---
+++ hackerrank/matrixRotation/matrixRot.py
@@ -0,0 +1,7 @@
+# Given a 2D Matrix, a, of dimension MxN and a positive integer R, you have to
+# rotate the matrix R times and print the resultant matrix. Rotation shoulld be in
+# an anti-clockwise directionself.
+#
+# https://www.hackerrank.com/challenges/matrix-rotation-algo
+
+import sys
