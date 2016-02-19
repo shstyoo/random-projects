e00f789fe0b11ed5a1a2d2b422a919eb45bde873
shstyoo <shstyoo@gmail.com>
2016-02-19 00:59:35 -0500

    fucking xcom

--- hackerrank/matrixRotation/matrixRot.py
+++ hackerrank/matrixRotation/matrixRot.py
@@ -1,7 +1,9 @@
 # Given a 2D Matrix, a, of dimension MxN and a positive integer R, you have to
 # rotate the matrix R times and print the resultant matrix. Rotation shoulld be in
-# an anti-clockwise direction. 
+# an anti-clockwise direction.
 #
 # https://www.hackerrank.com/challenges/matrix-rotation-algo
 
 import sys
+
+# fucking xcom 2
